6dffe2f41c73988fde0de6491df74e0e9d84fafa
givmefive5 <givme_five5@Yahoo.com>
2014-10-13 15:51:08 +0800

    for Christelle

---
+++ src/phr/dao/VerificationDao.java
@@ -0,0 +1,12 @@
+package phr.dao;
+
+import java.util.List;
+
+import phr.web.models.FBPost;
+
+public interface VerificationDao {
+
+	public void setNewUnverifiedPosts(String userAccessToken,
+			List<FBPost> newFbPosts);
+
+}
